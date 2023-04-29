80d1afc40332f6d104114e6507df287e1bb2361a
LarsEJonasson <47481796+LarsEJonasson@users.noreply.github.com>
2023-04-29 12:38:13 +0200

    enabled logging

--- src/main/java/org/example/RemoteChromeControl.java
+++ src/main/java/org/example/RemoteChromeControl.java
@@ -11,8 +11,25 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class RemoteChromeControl {
+    private static final Logger LOGGER = Logger.getLogger(RemoteChromeControl.class.getName());
+
+    static {
+        try {
+            FileHandler fileHandler = new FileHandler("RemoteChromeControl.log", true);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            LOGGER.addHandler(fileHandler);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Error setting up log file", e);
+        }
+    }
 
     public static void main(String[] args) {
         String os = System.getProperty("os.name").toLowerCase();
@@ -25,38 +42,37 @@ public class RemoteChromeControl {
         } else {
             throw new RuntimeException("Unsupported OS: " + os);
         }
-// Get login credentials from user
+        // Get login credentials from user
         GUI login = new GUI();
         String[] credentials = login.getLogin();
         String email = credentials[0];
         String password = credentials[1];
-
+        LOGGER.info("Starting Chrome driver");
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 
         ChromeOptions options = new ChromeOptions();
-        Map prefs=new HashMap();
+        Map<String, Object> prefs = new HashMap<>();
         prefs.put("profile.default_content_setting_values.notifications", 2); //Page notifications: 1-Allow, 2-Block, 0-default
-        options.setExperimentalOption("prefs",prefs);
+        options.setExperimentalOption("prefs", prefs);
         WebDriver driver = new ChromeDriver(options);
-
+        LOGGER.info("Navigating to Facebook login page");
         driver.get("https://www.facebook.com");
         // Wait until the page is fully loaded
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        LOGGER.info("Waiting for the page to load completely");
         wait.until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete'"));
+        LOGGER.info("Attempting to accept cookies");
         driver.manage().addCookie(new Cookie("cookiesAgreed", "true"));
         // Wait for the cookies popup to become visible
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]")));
-
-
+        LOGGER.info("Waiting for the cookies popup to become visible");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(., 'alla cookies') or contains(., 'all cookies')]")));
         // Click the cookie button
-        driver.findElement(By.cssSelector("button[data-cookiebanner='accept_button']")).click();
-
-
+        LOGGER.info("Clicking the 'Accept' button in the cookies popup");
+        driver.findElement(By.xpath("//button[contains(., 'alla cookies') or contains(., 'all cookies')]")).click();
         // Find the email and password fields and fill in the values
         driver.findElement(By.id("email")).sendKeys(email);
         driver.findElement(By.id("pass")).sendKeys(password);
-
         // Click the login
-       driver.findElement(By.name("login")).click();
+        driver.findElement(By.name("login")).click();
     }
 }
